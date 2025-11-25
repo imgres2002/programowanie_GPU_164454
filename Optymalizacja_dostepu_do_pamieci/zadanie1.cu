a1df6aa77ee09948d3143a1d950f5e753a75bff6
imgres <92025754+imgres2002@users.noreply.github.com>
2025-11-25 18:46:44 +0100

    Add files via upload

---
+++ Optymalizacja_dostepu_do_pamieci/zadanie1.cu
@@ -0,0 +1,55 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <iostream>
+
+#define a 2
+
+__global__ void ReduceInPlaceSharedMemory(float* input, int n) {
+    __shared__ float shared[256];
+
+    int tid = threadIdx.x;
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    shared[tid] = input[index] * a;
+
+    __syncthreads();
+
+    int start_idx = blockIdx.x * blockDim.x;
+    for(int i = 0; i < 256; i++){
+        input[start_idx+i] = shared[i];
+    }
+}
+
+int main() {
+    int n = 1024;
+    size_t bytes = n * sizeof(float);
+
+    float* h_input = new float[n];
+    float* d_input;
+
+    for (int i = 0; i < n; i++) {
+        h_input[i] = static_cast<float>(i + 1);
+    }
+
+    cudaMalloc(&d_input, bytes);
+
+    cudaMemcpy(d_input, h_input, bytes, cudaMemcpyHostToDevice);
+
+    int block_size = 256;
+    int grid_size = n / block_size;
+    // liczba elementow zgodna z liczba warp'ow w bloku
+    size_t shared_mem_size = (block_size / 32) * sizeof(float);
+
+    ReduceInPlaceSharedMemory <<<grid_size, block_size, shared_mem_size >>> (d_input, n);
+    cudaDeviceSynchronize();
+
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%f * %d = %f\n", h_input, a, d_input[i]);
+    }
+
+    cudaFree(d_input);
+    delete[] h_input;
+
+    return 0;
+}
